63f2d468a56e80ea2cd6f52dfbcffda7a7e8ebeb
Agustin Rangel <agustinran.ib@gmail.com>
2022-02-10 17:08:08 -0600

    Whole project, CRUD completed

---
+++ src/handlers/addTask.js
@@ -0,0 +1,36 @@
+const { v4 } = require("uuid");
+const AWS = require("aws-sdk");
+
+const addTask = async (event) => {
+	const dynamodb = new AWS.DynamoDB.DocumentClient();
+	const { title, description } = JSON.parse(event.body);
+	const createdAt = new Date();
+	const id = v4();
+
+	const newTask = {
+		id,
+		title,
+		description,
+		createdAt,
+		done: false,
+	};
+
+	await dynamodb
+		.put({
+			TableName: "TaskTable",
+			Item: newTask,
+		})
+		.promise();
+
+	console.log(dynamodb);
+	console.log(newTask);
+
+	return {
+		statusCode: 200,
+		body: JSON.stringify(newTask),
+	};
+};
+
+module.exports = {
+	addTask,
+};
---
+++ src/handlers/deleteTask.js
@@ -0,0 +1,24 @@
+const AWS = require("aws-sdk");
+
+const deleteTask = async (event) => {
+	const dynamodb = new AWS.DynamoDB.DocumentClient();
+	const { id } = event.pathParameters;
+
+	await dynamodb
+		.delete({
+			TableName: "TaskTable",
+			Key: { id },
+		})
+		.promise();
+
+	return {
+		status: 200,
+		body: {
+			message: "Task deleted succesfully",
+		},
+	};
+};
+
+module.exports = {
+	deleteTask,
+};
